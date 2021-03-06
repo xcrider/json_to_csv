fb0ea32ae5a7c497953ee37277fca30966a5730c
Karol Zawada <k.zawadaa@gmail.com>
2021-03-06 13:51:02 +0100

    Display result in HTML

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ import csv
 app = Flask(__name__)
 
 
-@app.route('/index', methods=['GET', 'POST'])
-def calculate():
-    if request.method == 'GET':
-       return render_template("index.html")
-    elif request.method == 'POST':
-       print("We received POST")
-       print(request.form)
-       return redirect("/")
 
 
 def update_exchange():
@@ -31,10 +23,11 @@ def update_exchange():
 
     #save into data.csv file
     with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns, delimiter=";")
-            writer.writeheader()
-            for data in convert:
-                writer.writerow(data)
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns, delimiter=";")
+        writer.writeheader()
+        for data in convert:
+            writer.writerow(data)
+
 
 # open csv file
 csv_file = "data.csv"
@@ -43,14 +36,22 @@ with open(csv_file, 'r') as csvfile:
     # get number of columns
     for line in csvfile.readlines():
         array = line.split('\n')
-        first_item = array[0]
 
     num_columns = len(array)
     csvfile.seek(0)
 
     reader = csv.reader(csvfile, delimiter=';')
-    included_cols = [1]
 
-    for row in reader:
-        currency_codes = list(row[i] for i in included_cols)
-        print(currency_codes)
+    currency_codes = {row[1]: row[3] for row in reader}
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def calculate():
+    if request.method == 'GET':
+        return render_template("index.html", currency_codes=list(currency_codes.keys())[1:])
+    elif request.method == 'POST':
+        print(request.form)
+        exchange_amount = int(request.form.get('value')) * float(currency_codes[request.form.get('currency')])
+        exchange_amount_r = round(exchange_amount, 2)
+        print(exchange_amount_r)
+        return render_template("/index.html", amount=exchange_amount_r)
